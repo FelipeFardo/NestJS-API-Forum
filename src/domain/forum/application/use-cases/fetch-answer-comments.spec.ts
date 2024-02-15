3adc417bbd448a93f8415f98850c86ae6c11767b
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-02-15 19:09:07 -0300

    chore: Copy Domain Layer and Creating the Infrastructure Layer

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswerComment } from 'test/factories/make-answer-comment'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch Answer Comments', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
+  })
+  it('should be able to fetch answer comments', async () => {
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-01'),
+      }),
+    )
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-01'),
+      }),
+    )
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-01'),
+      }),
+    )
+
+    const result = await sut.execute({
+      answerId: 'answer-01',
+      page: 1,
+    })
+    expect(result.value?.answerComments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated answer comments', async () => {
+    for (let i = 0; i < 22; i++) {
+      await inMemoryAnswerCommentsRepository.create(
+        makeAnswerComment({
+          answerId: new UniqueEntityID('answer-01'),
+        }),
+      )
+    }
+
+    const result = await sut.execute({
+      answerId: 'answer-01',
+      page: 2,
+    })
+    expect(result.value?.answerComments).toHaveLength(2)
+  })
+})
