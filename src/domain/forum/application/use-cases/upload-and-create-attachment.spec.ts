16796b94c5094c78584bb394ac598ef18717ad27
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-02-23 00:13:02 -0300

    feat: cloudflare integration r2, upload attachment controller and tests

--- src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
@@ -1,7 +1,7 @@
 import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
 import { UploadAndCreateAttachmentUseCase } from './upload-and-create-attachment'
 import { FakeUploader } from 'test/storage/fake-uploader'
-import { InvalidAttachmentType } from './errors/invalid-attachment-type'
+import { InvalidAttachmentType } from './errors/invalid-attachment-type-error'
 
 let inMemoryAttachmentRepository: InMemoryAttachmentsRepository
 let fakeUploader: FakeUploader
