de1cdf73c8e31f21f71506d0bbdb45a7bb2ff375
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-02-26 03:22:39 -0300

    feat: comment with author answer and tests

--- src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
@@ -38,7 +38,9 @@ describe('Fetch answer comments (E2E)', () => {
   })
 
   test('[GET] /questions/:questionId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const user = await studentFactory.makePrismaStudent({
+      name: 'John Doe',
+    })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -74,8 +76,14 @@ describe('Fetch answer comments (E2E)', () => {
     expect(response.statusCode).toEqual(200)
     expect(response.body).toEqual({
       comments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 01' }),
-        expect.objectContaining({ content: 'Comment 02' }),
+        expect.objectContaining({
+          content: 'Comment 01',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Comment 02',
+          authorName: 'John Doe',
+        }),
       ]),
     })
   })
